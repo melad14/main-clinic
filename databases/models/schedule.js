5e525ceeb4d637f7786faba0a7a55ed9233e33db
Milad Shehata <miladshehata513@gmial.com>
2024-07-10 20:57:42 +0300

    schedule

--- databases/models/schedule.js
+++ databases/models/schedule.js
@@ -3,12 +3,12 @@ import mongoose from 'mongoose';
 const scheduleSchema = new mongoose.Schema({
 
     date: {
-        type: Date,
+        type: String,
         required: true,
     },
     name: {
         type: String,
-        required: true,
+       
     },
     times: [{time: {
         type: String,
