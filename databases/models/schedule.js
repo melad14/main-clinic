b6b048cbe87d0d32c20e753d97e6e9c150d8dd83
Milad Shehata <miladshehata513@gmial.com>
2024-07-10 15:32:10 +0300

    schedule

--- databases/models/schedule.js
+++ databases/models/schedule.js
@@ -2,90 +2,19 @@ import mongoose from 'mongoose';
 
 const scheduleSchema = new mongoose.Schema({
 
-    sunday: {
-        times: [{
-            startTime: {
-                type: String,
-                required: true,
-            },
-            endTime: {
-                type: String,
-                required: true,
-            },
-        }],
+    date: {
+        type: Date,
+        required: true,
     },
-    monday: {
-          times: [{
-            startTime: {
-                type: String,
-                required: true,
-            },
-            endTime: {
-                type: String,
-                required: true,
-            },
-        }],
+    name: {
+        type: String,
+        required: true,
     },
-    tuesday: {
-          times: [{
-            startTime: {
-                type: String,
-                required: true,
-            },
-            endTime: {
-                type: String,
-                required: true,
-            },
-        }],
-    },
-    wednesday: {
-          times: [{
-            startTime: {
-                type: String,
-                required: true,
-            },
-            endTime: {
-                type: String,
-                required: true,
-            },
-        }],
-    },
-    thursday: {
-          times: [{
-            startTime: {
-                type: String,
-                required: true,
-            },
-            endTime: {
-                type: String,
-                required: true,
-            },
-        }],
-    },
-    friday: {
-          times: [{
-            startTime: {
-                type: String,
-                required: true,
-            },
-            endTime: {
-                type: String,
-                required: true,
-            },
-        }],
-    },
-    saturday: {
-          times: [{
-            startTime: {
-                type: String,
-                required: true,
-            },
-            endTime: {
-                type: String,
-                required: true,
-            },
-        }],
-    },
+    times: [{time: {
+        type: String,
+        required: true,
+    }}]
+    
 }, { timestamps: true });
 
 export const Schedule = mongoose.model('schedule', scheduleSchema);
