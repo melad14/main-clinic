e87d4b34173c1eeeb6c474fc1269e3cc9856a853
Milad Shehata <miladshehata513@gmial.com>
2024-07-28 16:27:13 +0300

    commit

--- src/modules/auth/auth.router.js
+++ src/modules/auth/auth.router.js
@@ -1,6 +1,8 @@
 import express from 'express'
 import { completeProfile, getDoctor, profile, signIn, verifyOTP } from './auth.controller.js';
 import { allowTo, protectedRoutes } from '../../middleware/protectedRoute.js';
+import { validation } from './../../middleware/validation.js';
+import { signInSchema, verifySchema } from './auth.validation.js';
 
 
 
@@ -8,7 +10,8 @@ import { allowTo, protectedRoutes } from '../../middleware/protectedRoute.js';
 const userRouter = express.Router();
 
 
-userRouter.post('/signin', signIn);
+userRouter.post('/signin', validation(signInSchema),signIn);
+userRouter.post('/signin', validation(verifySchema),signIn);
 userRouter.post('/verify-otp', verifyOTP);
 userRouter.put('/complete-profile',protectedRoutes, allowTo('user') ,completeProfile);
 userRouter.get('/profile',protectedRoutes, allowTo('user'), profile);
