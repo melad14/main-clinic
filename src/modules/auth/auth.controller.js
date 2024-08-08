309b7fd9a5db0d7344aea2ec7c293a97058adbdc
Milad Shehata <miladshehata513@gmial.com>
2024-08-08 18:11:55 +0300

    test

--- src/modules/auth/auth.controller.js
+++ src/modules/auth/auth.controller.js
@@ -6,39 +6,44 @@ import { sendSMSTest } from '../../emails/user.sms.js';
 import { AppErr } from '../../utils/AppErr.js';
 import { catchAsyncErr } from './../../utils/catcherr.js';
 import jwt from "jsonwebtoken"
+import { Reservation } from './../../../databases/models/reservation.js';
+import { thalilModel } from './../../../databases/models/tahalil.js';
+import { roshtaModel } from './../../../databases/models/roshtat.js';
+import { medicinModel } from '../../../databases/models/medicin.js';
+import { asheaaModel } from '../../../databases/models/asheaa.js';
 
 const generateOTP = () => {
     return Math.floor(100000 + Math.random() * 900000).toString(); // Generate a 6-digit OTP
 };
 
 const signIn = catchAsyncErr(async (req, res, next) => {
-    
+
     const { phone } = req.body;
     let user = await userModel.findOne({ phone });
     if (user && user.blocked) {
         return next(new AppErr("User is blocked", 403));
-      }
+    }
     if (!user) {
-        const user = new userModel({ phone ,fullName:''})
+        const user = new userModel({ phone, fullName: '' })
         await user.save()
 
         const otp = generateOTP();
         const otpExpires = new Date(Date.now() + 10 * 60000); // OTP valid for 10 minutes
         await userModel.updateOne({ phone }, { otp, otpExpires });
         await sendSMSTest(phone, `Your OTP is ${otp}`);
-    
+
         res.status(200).json({ "message": "user created and OTP sent" });
     }
-else{
-    const otp = generateOTP();
-    const otpExpires = new Date(Date.now() + 10 * 60000); // OTP valid for 10 minutes
-    await userModel.updateOne({ phone }, { otp, otpExpires });
+    else {
+        const otp = generateOTP();
+        const otpExpires = new Date(Date.now() + 10 * 60000); // OTP valid for 10 minutes
+        await userModel.updateOne({ phone }, { otp, otpExpires });
 
-    await sendSMSTest(phone, `Your OTP is ${otp}`);
+        await sendSMSTest(phone, `Your OTP is ${otp}`);
 
-    res.status(200).json({ "message": "OTP sent" });
-}
-   
+        res.status(200).json({ "message": "OTP sent" });
+    }
+
 });
 const verifyOTP = catchAsyncErr(async (req, res, next) => {
     const { phone, otp } = req.body;
@@ -56,7 +61,7 @@ const verifyOTP = catchAsyncErr(async (req, res, next) => {
 
 const completeProfile = catchAsyncErr(async (req, res, next) => {
 
-    const result = await userModel.findByIdAndUpdate( req.user._id , req.body, { new: true });
+    const result = await userModel.findByIdAndUpdate(req.user._id, req.body, { new: true });
     if (!result) return next(new AppErr('Error complete profile', 200));
     res.status(200).json({ message: "success", result });
 
@@ -64,7 +69,7 @@ const completeProfile = catchAsyncErr(async (req, res, next) => {
 
 
 export const profile = catchAsyncErr(async (req, res, next) => {
-  
+
 
     const users = await userModel.findById(req.user._id)
         .populate('tahalil')
@@ -78,25 +83,36 @@ export const profile = catchAsyncErr(async (req, res, next) => {
 });
 
 export const deleteAcc = catchAsyncErr(async (req, res, next) => {
-     await userModel.findByIdAndDelete(req.user._id)
+    const userId = req.user._id;
+
+
+    await Reservation.deleteMany({ user: userId });
+    await thalilModel.deleteMany({ userid: userId });
+    await roshtaModel.deleteMany({ userid: userId });
+    await medicinModel.deleteMany({ userid: userId });
+    await asheaaModel.deleteMany({ userid: userId });
+    await conversationModel.deleteMany({ participants: userId });
+    // Delete the user
+    await userModel.findByIdAndDelete(userId);
+
     res.status(200).json({ message: "success" });
 
 });
 
 export const getDoctor = catchAsyncErr(async (req, res, next) => {
-    const doctor=await adminModel.find()
-  
+    const doctor = await adminModel.find()
+
     res.status(200).json({ message: "success", doctor });
 
 });
 export const getDoctorInfo = catchAsyncErr(async (req, res, next) => {
-    const doctorInfo=await doctorInfoModel.find()
-  
+    const doctorInfo = await doctorInfoModel.find()
+
     res.status(200).json({ message: "success", doctorInfo });
 
 });
 
 
 export {
-    signIn,verifyOTP,completeProfile
+    signIn, verifyOTP, completeProfile
 }
