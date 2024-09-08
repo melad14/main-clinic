6dd756aac49dac5b083a256c36bcdc773432b783
Milad Shehata <miladshehata513@gmial.com>
2024-09-08 17:52:41 +0300

    upload

--- src/modules/medicalReports/medicalReport.router.js
+++ src/modules/medicalReports/medicalReport.router.js
@@ -32,9 +32,9 @@ reportRouter.post('/upload-medicin', protectedRoutes, allowTo('admin'),
 // edit ////////////////////////////////
 
 reportRouter.put('/edit-roshta/:id', protectedRoutes, allowTo('admin','user'),
-  upload.fields([{ name: 'image', maxCount: 5 },{ name: 'pdf', maxCount: 5 }]), editRoshta);
+  upload.fields([{ name: 'image', maxCount: 5 }]), editRoshta);
 reportRouter.put('/edit-tahlil/:id', protectedRoutes, allowTo('admin','user'),
-  upload.fields([{ name: 'image', maxCount: 5 }]), editTahlil);
+  upload.fields([{ name: 'image', maxCount: 5 },{ name: 'pdf', maxCount: 5 }]), editTahlil);
 reportRouter.put('/edit-medicin/:id', protectedRoutes, allowTo('admin','user'),
   upload.fields([{ name: 'image', maxCount: 5 }]), editMedicin);
 reportRouter.put('/edit-ashe3a/:id', protectedRoutes, allowTo('admin','user'),
